75f5c51627e589986ade3c901406192f33586fab
Hyun <121593967+cHynekim@users.noreply.github.com>
2024-07-24 23:34:55 +1000

    Title: 입력받아 출력 2, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240724/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int id_num1, id_num2;
+    cin >> id_num1;
+    cin.get();
+    cin >> id_num2;
+    cout << id_num1 << id_num2;
+    return 0;
+}
