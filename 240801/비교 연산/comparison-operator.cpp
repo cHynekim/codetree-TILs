a1df70391f23355a012011a30218aeeb1b97c448
Hyun <121593967+cHynekim@users.noreply.github.com>
2024-08-01 03:59:45 +1000

    Title: 비교 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240801/비교 연산/comparison-operator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+    cin >> a >> b;
+
+    cout << (a >= b) << '\n';
+    cout << (a > b) << '\n';
+    cout << (b >= a) << '\n';
+    cout << (b > a) << '\n';
+    cout << (a == b) << '\n';
+    cout << (a != b);
+    return 0;
+}
