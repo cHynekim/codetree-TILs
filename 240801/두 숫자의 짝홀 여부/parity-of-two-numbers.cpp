a1b7b9edf54793f2705dad6eae515c8b40c97422
Hyun <121593967+cHynekim@users.noreply.github.com>
2024-08-01 03:36:27 +1000

    Title: 두 숫자의 짝홀 여부, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240801/두 숫자의 짝홀 여부/parity-of-two-numbers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+    cin >> a >> b;
+
+    if(a % 2){
+        cout << "odd\n";
+    }
+    else cout << "even\n";
+
+    
+    if(b % 2){
+        cout << "odd";
+    }
+    else cout << "even";
+
+    return 0;
+}
