ef55b52c0a1bb6a060189a50a9a7f613033b4a28
Hyun <121593967+cHynekim@users.noreply.github.com>
2024-08-01 02:50:08 +1000

    Title: 등급 매기기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240801/등급 매기기/ranking.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int scr;
+    cin >> scr;
+
+    if(scr >= 90){
+        cout << 'A';
+    }
+    else if(scr < 90 & scr >= 80){
+        cout << 'B';
+    }
+    else if(scr < 80 & scr >= 70){
+        cout << 'C';
+    }
+    else if(scr < 70 & scr >= 60){
+        cout << 'C';
+    }
+    else cout << 'F';
+    
+    return 0;
+}
