0216106097855b3e318d23b3cd01b8fec9bbc9e7
Hyun <121593967+cHynekim@users.noreply.github.com>
2024-08-01 17:53:20 +1000

    Title: 중앙값 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240801/중앙값 구하기/find-the-median.cpp
+++ 240801/중앙값 구하기/find-the-median.cpp
@@ -7,18 +7,22 @@ int main() {
 
     cin >> a >> b >> c;
 
-    if(a > b){ // 1 3
-        if(c > a) // 5 1
+    if(a > b){
+        if(c > a) // c > a > b
             cout << a;
-        else
-            cout << b;
-    }
-    else{ 
-        if(b < c)
+        else if(b > c)// a > b / c
             cout << b;
         else
             cout << c;
     }
-    
+    else{ 
+        if(b < c) // c > b > a
+            cout << b;
+        else if(a < c)// b > a / c
+            cout << c;
+        else
+            cout << a;
+    }
+
     return 0;
 }
